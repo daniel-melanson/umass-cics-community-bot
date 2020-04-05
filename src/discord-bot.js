7b5d74c44cc4056b29a5e1df92daad7c15fe0367
Daniel Melanson <0dmelanson@gmail.com>
2020-04-05 10:05:33 -0400

    Removed verify-command.js, switched verification to reaction based

--- src/discord-bot.js
+++ src/discord-bot.js
@@ -9,6 +9,7 @@ const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 const path = require('path');
 const util = require('./util.js');
+const { oneLine } = require("common-tags");
 
 // Constants for embed generation
 const EMBED_CONSTANTS = {
@@ -74,7 +75,7 @@ class DiscordBot extends Commando.Client {
             fields: options.fields,
             image: options.image,
             title: options.title,
-            timestamp: new Date()
+            footer: options.footer
         });
 
         if (options.author)
@@ -82,7 +83,10 @@ class DiscordBot extends Commando.Client {
         else
             embed.setAuthor(this.user.username, this.user.avatarURL());
 
-        embed.setColor(options.hexColor || EMBED_CONSTANTS.COLOR);
+        embed.setColor(options.hexColor ? options.hexColor : EMBED_CONSTANTS.COLOR);
+
+        if (options.time)
+            embed.setTimestamp(new Date());
 
         return {embed: embed};
     }
@@ -108,9 +112,84 @@ class DiscordBot extends Commando.Client {
     async ready() {
         console.log(`Logged in as ${this.user.tag}`);
 
-        let channels = this.guilds.find(g => g.name === "UMass CICS Community").channels;
+        this.guild = this.guilds.get("574287921717182505");
 
-        this.log = channels.find(c => c.name === "bot-log");
+        if (!this.guild) {
+            console.log("Unable to find main guild");
+            this.destroy();
+        }
+
+        let channels = this.guild.channels;
+        this.log = channels.find(c => c.name === "bot-log" && c.type === "text");
+        this.welcome = channels.find(c => c.name === "welcome" && c.type === "text");
+
+        this.setupVerification();
+    }
+
+
+    /**
+     * Sets up user verification
+     * @returns {Promise<void>}
+     */
+    async setupVerification() {
+        let channel = this.welcome;
+        let verifiedRole = this.guild.roles.find(r => r.name === "Verified");
+        let botCommands = this.guild.channels.find(c => c.name === 'bot-commands');
+
+        await channel.bulkDelete(await this.welcome.messages.fetch());
+
+        let verifyMessage = await channel.send(this.generateEmbed({
+            title: "Welcome to the University of Massachusetts College of Information and Computer Science Community Discord Server!",
+            description: oneLine(`This server is a gathering of UMass CICS prospects, students, and alumni. 
+                Members can discuss the material of a current class, show off their extracurricular projects, or talk about issues facing the computer science community. 
+                To help foster this community, **we ask all of our members to associate themselves with a real-life name.** 
+                Gamer-tags are pretty neat, but it gets very confusing when we have a community of hundreds of people. 
+                **If you are uncomfortable with using your formal first name, we encourage you to use a pet name or nickname.** 
+                To get started, follow the instructions based on your device:`),
+            fields: [
+                {
+                    name: "*Desktop*",
+                    value: "Click on `UMass CICS Community` in bold in the top left of your screen. Press `Change Nickname`, enter your identifier, and `Save`.",
+                    inline: true
+                },
+                {
+                    name: "*Mobile*",
+                    value: "Swipe to the right to display your sever list. Press the three vertically aligned dots next to `UMass CICS Community`. Press `Change Nickname`, enter your identifier, and `Save`.",
+                    inline: true
+                },
+                {
+                    name: "**Next Steps**",
+                    value: oneLine(`**Once you are done setting your nickname, react to this message with the check-mark.**
+                    This tells the bot that you are ready to enter the server. Keep in mind that we track the users that verify.
+                    If you have an unfavorable name, you are subject to moderation. Once you verify, the bot will be waiting for you in the #bot-commands channel for next steps. Enjoy!`),
+                }
+            ],
+            footer: {
+                icon_url: this.user.avatarURL(),
+                test: "UMass CICS Community"
+            },
+            time: true,
+        }));
+
+        let collector = verifyMessage.createReactionCollector((reaction, user) => user.id !== this.user.id, {
+            dispose: true
+        });
+
+        collector.on('collect', async (r, user) => {
+            let guildMember = await this.guild.members.fetch(user);
+
+            if (guildMember) {
+                await guildMember.roles.add(verifiedRole);
+
+                await this.registry.resolveCommand('roles').fn({
+                    author: user,
+                    guild: this.guild,
+                    fakeChannel: botCommands
+                });
+            }
+        });
+
+        await verifyMessage.react('✅');
     }
 
     /**
