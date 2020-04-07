9af96d346530c5c3f438fdc0748957ffa04eb461
Daniel Melanson <0dmelanson@gmail.com>
2020-04-07 09:59:48 -0400

    Added say-command.js

---
+++ src/commands/admin/say-command.js
@@ -0,0 +1,50 @@
+/**
+ * @author Daniel Melanson
+ * @date 4/6/2020
+ * @desc Source file for `!say` command class
+ */
+
+// Modules
+const Command = require('../command');
+
+/**
+ * @desc SayCommand singleton that defines behavior for the `!say` command.
+ */
+class SayCommand extends Command {
+    /**
+     * Default constructor for SayCommand object.
+     * @param client The discord-js-commando.Client class that the command is registered to
+     */
+    constructor(client) {
+        super(client, {
+            name: 'say',
+            group: 'admin',
+            properName: 'say',
+            description: 'Prints an embedded message with the server rules.',
+            userPermissions: ['ADMINISTRATOR'],
+            clientPermissions: ['SEND_MESSAGES'],
+            examples: ['!say Hello!'],
+            args: [
+                {
+                    key: 'phrase',
+                    label: 'phrase',
+                    prompt: 'What would you like me to say?',
+                    type: 'string',
+                    infinite: true,
+                }
+            ]
+        });
+    }
+
+    /**
+     * Repeats the contents of the requesting message.
+     * @param msg The message that requested the command
+     * @param args The arguments of the message
+     * @returns {Promise<Message|Message[]|*>}
+     */
+    async fn(msg, args) {
+        return msg.channel.send(args.phrase.join(' '));
+    }
+}
+
+module.exports = SayCommand;
