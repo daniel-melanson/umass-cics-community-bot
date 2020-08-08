832acf17875880b30b1012544885e11d2f1be7c5
Larry Tseng <larry.tseng.1@gmail.com>
2020-08-08 15:25:13 -0400

    Add zoomer role

--- src/util.js
+++ src/util.js
@@ -19,7 +19,7 @@ function isClass(str) {
  * @returns {boolean}
  */
 function isResidential(str) {
-    return new RegExp(/^(central|ohill|northeast|southwest|honors|sylvan|off-campus|rap data science|rap ethics society)$/i).test(str.trim());
+    return new RegExp(/^(zoomer|central|ohill|northeast|southwest|honors|sylvan|off-campus|rap data science|rap ethics society)$/i).test(str.trim());
 }
 
 /**
