14d033c595e16cc41aafd5ba37d8de04ca407d4b
Daniel Melanson <0dmelanson@gmail.com>
2020-11-08 06:04:49 -0500

    Add getCourseIdFromQuery and getCourseDependents

--- src/UMass/courses.ts
+++ src/UMass/courses.ts
@@ -1,12 +1,26 @@
-import { Course } from "UMass/types";
 import { courseMap } from "UMass/data";
 
-export function getCourseIdFromQuery(): string | undefined {
+export function getCourseIdFromQuery(query: string): string | undefined {
+	const match = query
+		.trim()
+		.toUpperCase()
+		.match(/^(cs|math|stat|cics|info|compsci)\s*\d{3}[a-z]*$/i);
 
+	if (match === null) return undefined;
+
+	return `${match[0].replace("CS", "COMPSCI")} ${match[1]}`;
 }
 
-export function getCourseDependents(id: string): Array<Course> | undefined {
+export function getCourseDependents(id: string): Array<string> {
 	if (courseMap === undefined) throw new Error("Unable to get UMass data");
 
-	
+	const dependents = [];
+
+	for (const course of courseMap.values()) {
+		if (course.prerequisites !== undefined && course.prerequisites.has(id)) {
+			dependents.push(course.id);
+		}
+	}
+
+	return dependents;
 }
