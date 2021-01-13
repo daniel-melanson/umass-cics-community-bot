73493fc563c80141e3360285839493b8d3378f5f
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-01-13 16:06:35 -0500

    Update login logic

--- src/Discord/server.ts
+++ src/Discord/server.ts
@@ -28,26 +28,36 @@ client.on("message", async (message: Message) => {
 	handleCommandMessage(client, message);
 });
 
-export async function login(token: string): Promise<void> {
-	await client.login(token);
+export function login(token: string): Promise<void> {
+	return new Promise<void>((res, rej) => {
+		client.on("ready", () => res());
 
-	console.log(`Logged in as ${client.user?.tag}`);
+		client.login(token);
+	}).then(async () => {
+		console.log(`Logged in as ${client.user?.tag}`);
 
-	const guild = await client.guilds.fetch(process.env["DISCORD_GUILD_ID"]!);
-	const sendChannel = async (name: string, embeds: MessageEmbed | Array<MessageEmbed>) => {
-		const channel = guild.channels.cache.find(x => x.type === "text" && x.name === name) as TextChannel | undefined;
+		const guild = await client.guilds.fetch(process.env["DISCORD_GUILD_ID"]!);
+		const sendChannel = async (name: string, embeds: MessageEmbed | Array<MessageEmbed>) => {
+			const channel = guild.channels.cache.find(x => x.type === "text" && x.name === name) as
+				| TextChannel
+				| undefined;
 
-		if (channel) {
-			await channel.bulkDelete(20);
-			await channel.send(embeds);
-		}
-	};
+			if (channel) {
+				await channel.bulkDelete(20);
+				if (embeds instanceof Array) {
+					for (const embed of embeds) await channel.send(embed);
+				} else {
+					await channel.send(embeds);
+				}
+			}
+		};
 
-	await sendChannel("rules", DISCORD_RULES);
-	await sendChannel("how-to-roles", ROLES_TUTORIAL);
-	await sendChannel("how-to-notifications", NOTIFICATION_TUTORIALS);
-	await sendChannel("how-to-commands", COMMANDS_TUTORIAL);
-	await sendChannel("welcome", WELCOME_MESSAGES);
+		await sendChannel("rules", DISCORD_RULES);
+		await sendChannel("how-to-roles", ROLES_TUTORIAL);
+		await sendChannel("how-to-notifications", NOTIFICATION_TUTORIALS);
+		await sendChannel("how-to-commands", COMMANDS_TUTORIAL);
+		await sendChannel("welcome", WELCOME_MESSAGES);
+	});
 }
 
 export async function announce(channel: "general" | "university", message: string | MessageEmbed): Promise<void> {
