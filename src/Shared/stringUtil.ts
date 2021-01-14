ff25794663055c2e88c21e285ef022801fc2f9dd
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-01-14 18:22:34 -0500

    Remove infinite loop 😬

--- src/Shared/stringUtil.ts
+++ src/Shared/stringUtil.ts
@@ -1,7 +1,7 @@
 export function sanitize(str: string): string {
 	str = str.toLowerCase().trim();
 
-	while (str.search(/\s\s/)) str = str.replace(/\s\s/g, " ");
+	while (str.search(/\s\s/) != -1) str.replaceAll(/\s\s/, " ");
 
 	return str;
 }
