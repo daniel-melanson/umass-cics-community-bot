bff180101d1b791e6e55b6dc2c660a5eac312afd
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-01-21 22:36:09 -0500

    Fix role command

--- src/Discord/commands/roles/role.ts
+++ src/Discord/commands/roles/role.ts
@@ -1,4 +1,5 @@
 import { Client, Message, Role } from "discord.js";
+import { oneLine } from "Shared/stringUtil";
 
 import { Command } from "../types";
 
@@ -17,32 +18,44 @@ export default {
 		},
 	],
 	func: async (client: Client, message: Message, result: { roles: Array<Role> }) => {
-		const roleList = result["roles"];
+		const roleList = result!["roles"];
 
-		const added = [];
-		const removed = [];
+		const rolesAdded = [];
+		const rolesRemoved = [];
+
 		const roleManager = message.member!.roles;
+		const updatedRoles = roleManager.cache.clone();
 		for (const role of roleList) {
-			if (roleManager.cache.has(role.id)) {
-				removed.push(role.name);
-				await roleManager.remove(role);
-			} else if (role.permissions.valueOf() === 0) {
-				added.push(role.name);
-				await roleManager.add(role);
-			} else {
+			if (role.permissions.valueOf() !== 0) {
 				message.reply(
-					`unable to give you the ${role.name} role. Role contains permissions. Contact an administrator.`,
+					oneLine(`the ${role.name} role contains permissions.
+					You are not allowed to manage roles with permissions.
+					If you believe this is a mistake, contact an administrator.`),
 				);
+			} else if (updatedRoles.has(role.id)) {
+				updatedRoles.delete(role.id);
+				rolesRemoved.push(role.name);
+			} else {
+				updatedRoles.set(role.id, role);
+				rolesAdded.push(role.name);
 			}
 		}
 
+		try {
+			await roleManager.set(updatedRoles);
+		} catch (e) {
+			return message.reply("I encountered an error while trying to update your roles. Try again later.");
+		}
+
+		const addedList = rolesAdded.join(", ");
+		const removedList = rolesRemoved.join(", ");
 		let content = "your roles have been updated.";
-		if (added.length === 0) {
-			content += `You no longer have: ${removed.join()}.`;
-		} else if (removed.length === 0) {
-			content += `You now have: ${removed.join()}.`;
-		} else {
-			content += `You now have: ${removed.join()} and no longer have ${removed.join()}.`;
+		if (rolesAdded.length === 0 && rolesRemoved.length > 0) {
+			content += ` You no longer have: ${removedList}.`;
+		} else if (rolesRemoved.length === 0 && rolesAdded.length > 0) {
+			content += ` You now have: ${addedList}.`;
+		} else if (rolesAdded.length + rolesRemoved.length) {
+			content += ` You now have: ${addedList} and no longer have ${removedList}.`;
 		}
 
 		return message.reply(content);
