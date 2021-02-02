4524e7f23968bdbd6c31449c2eb735fab384025e
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-02-01 20:27:04 -0500

    Add accept and reject commands

---
+++ src/Discord/commands/admin/accept.ts
@@ -0,0 +1,50 @@
+import { Message, TextChannel } from "discord.js";
+
+import { Command, UserPermission } from "Discord/commands/types";
+import { formatEmbed } from "Discord/formatting";
+
+export default {
+	identifier: "accept",
+	aliases: ["accept-suggestion"],
+	group: "Administrative",
+	description: "Creates and posts an accepted suggestion embed.",
+	arguments: [
+		{
+			name: "suggestion",
+			prompt: "what was the suggestion?",
+			type: "string",
+		},
+		{
+			name: "note",
+			prompt: "why was it accepted?",
+			type: "string",
+		},
+	],
+	userPermission: UserPermission.Moderator,
+	guildOnly: true,
+	func: async (message: Message, result: { suggestion: string; note: string }) => {
+		const guild = message.guild!;
+		const suggestionChannel = guild.channels.cache.find(ch => ch.name === "suggestion-log") as
+			| TextChannel
+			| undefined;
+
+		if (!suggestionChannel) return message.reply("I can't seem to find the suggestion channel.");
+
+		return suggestionChannel.send(
+			formatEmbed({
+				title: "Suggestion Rejected",
+				fields: [
+					{
+						name: "Suggestion",
+						value: `*${result.suggestion}*`,
+					},
+					{
+						name: "Note",
+						value: result.note,
+					},
+				],
+				color: "#2ecc71",
+			}),
+		);
+	},
+} as Command;
