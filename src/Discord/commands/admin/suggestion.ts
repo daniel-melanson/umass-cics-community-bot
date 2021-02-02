4e03fb5aa0f3d7e509a1ba80ddc5679d9deab6dd
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-02-02 13:55:17 -0500

    Remove reject and accept in favor of suggestion

--- src/Discord/commands/admin/suggestion.ts
+++ src/Discord/commands/admin/suggestion.ts
@@ -4,30 +4,34 @@ import { Command, UserPermission } from "Discord/commands/types";
 import { formatEmbed } from "Discord/formatting";
 
 export default {
-	identifier: "reject",
-	aliases: ["reject-suggestion"],
+	identifier: "suggestion",
+	aliases: ["suggest", "sugg"],
 	group: "Administrative",
-	description: "Creates and posts a rejected suggestion embed.",
-	arguments: [
+	description: "Creates and posts an suggestion embed.",
+	parameters: [
 		{
 			name: "suggestion",
 			prompt: "what was the suggestion?",
 			type: "string",
 		},
 		{
+			name: "conclusion",
+			prompt: "was it accepted or rejected?",
+			type: "string",
+		},
+		{
 			name: "note",
-			prompt: "why was it rejected?",
+			prompt: "why did you reach this conclusion?",
 			type: "string",
 		},
 	],
 	userPermission: UserPermission.Moderator,
 	guildOnly: true,
-	func: async (message: Message, result: { suggestion: string; note: string }) => {
+	func: async (message: Message, result: { conclusion: string; suggestion: string; note: string }) => {
 		const guild = message.guild!;
 		const suggestionChannel = guild.channels.cache.find(ch => ch.name === "suggestion-log") as
 			| TextChannel
 			| undefined;
-
 		if (!suggestionChannel) return message.reply("I can't seem to find the suggestion channel.");
 
 		return suggestionChannel.send(
@@ -43,7 +47,7 @@ export default {
 						value: result.note,
 					},
 				],
-				color: "#2ecc71",
+				color: result.conclusion.match(/^r/i) ? "#e74c3c" : "#2ecc71",
 			}),
 		);
 	},
