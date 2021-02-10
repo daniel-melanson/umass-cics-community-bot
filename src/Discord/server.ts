93579312cbbda7f9d9d390e4f9c5b57e85afc679
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-02-10 22:47:31 +0000

    Update verify system

--- src/Discord/server.ts
+++ src/Discord/server.ts
@@ -1,25 +1,12 @@
-import {
-	Client,
-	Emoji,
-	Guild,
-	GuildMember,
-	Message,
-	MessageEmbed,
-	MessageOptions,
-	MessageReaction,
-	TextChannel,
-	User,
-} from "discord.js";
-import { oneLine } from "Shared/stringUtil";
+import { Client, Guild, GuildMember, Message, MessageEmbed, MessageOptions, TextChannel } from "discord.js";
 
 import { NOTIFICATION_TUTORIALS } from "Discord/constants/how-to-notifications";
 import { ROLES_TUTORIAL } from "Discord/constants/how-to-roles";
-import { WELCOME_MESSAGES } from "Discord/constants/welcome";
 import { DISCORD_RULES } from "Discord/constants/rules";
 
 import { handleCommandMessage } from "Discord/dispatcher";
-import { formatEmbed } from "Discord/formatting";
-import { CONTACT_MESSAGE } from "Discord/constants";
+import { oneLine } from "Shared/stringUtil";
+import { formatEmbed } from "./formatting";
 
 const DISCORD_GUILD_ID = process.env["DISCORD_GUILD_ID"]!;
 if (!DISCORD_GUILD_ID) {
@@ -48,126 +35,6 @@ export async function announce(
 	await sendingChannel.send(message);
 }
 
-async function handleVerify(reaction: MessageReaction, user: User) {
-	const guild = reaction.message.guild!;
-	const member = await guild.members.fetch(user);
-	const roleManager = await guild.roles.fetch();
-	const verifiedRole = roleManager.cache.find(role => role.name === "Verified");
-
-	if (verifiedRole) {
-		try {
-			await member.roles.add(verifiedRole);
-		} catch (e) {
-			console.error("[SERVER-VERIFICATION] Unable to manage user permissions:", e);
-			return announce(
-				"bot-log",
-				formatEmbed({
-					title: "Error",
-					description: `I can't seem to manage your permissions. ${CONTACT_MESSAGE}`,
-					color: "#c0392b",
-				}),
-			);
-		}
-
-		announce(
-			"bot-log",
-			formatEmbed({
-				author: user,
-				description: oneLine(`<@${member.user.id}> has just been verified.
-						Their identifier is **${member.nickname || member.user.username}**.
-						Their account was created on ${user.createdAt.toLocaleDateString()}`),
-				color: "#2ecc71",
-			}),
-		);
-
-		const get = (name: string) => {
-			const channel = findChannel(guild, `how-to-${name}`);
-			if (!channel) return "";
-
-			return `<#${channel.id}>`;
-		};
-
-		announce("bot-commands", {
-			content: `Hey there, <@${member.id}>!`,
-			embed: formatEmbed({
-				title: `Welcome to the Server!`,
-				fields: [
-					{
-						name: "Getting Familiar With The Server",
-						value: oneLine(`If you are unfamiliar with the server,
-									make sure to read the how-to channels (${get("roles")}, ${get("commands")}, ${get("notifications")})`),
-					},
-					{
-						name: "Obtaining Roles to Gain Access to Channels",
-						value: oneLine(`You can assign yourself some roles using this [website](https://discord.ltseng.me/)
-									or using the \`!role\` command. To view a list of all assignable roles,
-									you can use the \`!roles\` command.`),
-					},
-				],
-			}),
-		});
-	} else {
-		console.error("[DISCORD] Unable to find Verified role.");
-	}
-}
-
-export function login(token: string): Promise<void> {
-	return new Promise<void>((res, rej) => {
-		client.on("ready", () => res());
-
-		client.login(token).catch(error => rej(error));
-	}).then(async () => {
-		console.log(`Logged in as ${client.user?.tag}`);
-
-		await client.user?.setActivity({
-			type: "LISTENING",
-			name: "!help",
-		});
-
-		const guild = await client.guilds.fetch(DISCORD_GUILD_ID);
-		const botMember = await guild.members.fetch(client.user!);
-		if (!botMember.permissions.has("ADMINISTRATOR")) {
-			console.error("[SERVER] Insignificant permissions in guild.");
-			process.exit(-1);
-		}
-
-		const sendChannel = async (name: string, embeds: MessageEmbed | Array<MessageEmbed | string>) => {
-			let channel;
-			if ((channel = findChannel(guild, name))) {
-				await channel.bulkDelete(20);
-				if (embeds instanceof Array) {
-					for (const embed of embeds) await channel.send(embed);
-				} else {
-					await channel.send(embeds);
-				}
-			}
-		};
-
-		if (process.env["SEND_MESSAGES"]) {
-			await sendChannel("rules", DISCORD_RULES);
-			await sendChannel("how-to-roles", ROLES_TUTORIAL);
-			await sendChannel("how-to-notifications", NOTIFICATION_TUTORIALS);
-		}
-
-		const welcome = findChannel(guild, "welcome");
-		if (!welcome) throw new Error("Unable to find welcome channel");
-		await welcome.bulkDelete(10);
-		for (let i = 0; i < WELCOME_MESSAGES.length; i++) {
-			const msg = await welcome.send(WELCOME_MESSAGES[i]);
-
-			if (i === WELCOME_MESSAGES.length - 2) {
-				await msg.react("✅");
-
-				const collector = msg.createReactionCollector(
-					(reaction: MessageReaction) => reaction.emoji.name === "✅",
-				);
-
-				collector.on("collect", handleVerify);
-			}
-		}
-	});
-}
-
 async function handleCarrotUpvote(message: Message) {
 	const content = message.content;
 	if (message.guild && message.deletable && content === "^") {
@@ -192,15 +59,15 @@ async function handleCarrotUpvote(message: Message) {
 			}
 		}
 
+		message.delete();
 		return true;
 	}
 }
 
-client.on("message", async (message: Message) => {
+async function message(message: Message) {
 	if (message.partial || message.author.bot) return;
 
 	if (await handleCarrotUpvote(message)) {
-		if (message.deletable) message.delete();
 		return;
 	}
 
@@ -216,8 +83,130 @@ client.on("message", async (message: Message) => {
 
 		await message.react(upvote);
 		await message.react(downvote);
+
 		return;
 	}
 
 	handleCommandMessage(message);
-});
+}
+
+async function guildMemberAdd(member: GuildMember) {
+	if (member.guild.id !== DISCORD_GUILD_ID) return;
+
+	const id = member.id;
+
+	await member.setNickname("real name please");
+	await announce(
+		"bot-log",
+		formatEmbed({
+			author: member.user,
+			description: oneLine(`<@${member.user.id}> has joined.
+						Their account was created on ${member.user.createdAt.toLocaleDateString()}`),
+			color: "#2ecc71",
+		}),
+	);
+
+	setTimeout(async () => {
+		let updated;
+		try {
+			updated = await member.guild.members.fetch({ user: id, force: true });
+		} catch (e) {
+			return;
+		}
+
+		if (updated.nickname !== "real name please" && updated.roles.cache.size > 1) return;
+
+		const get = (name: string) => {
+			const channel = findChannel(member.guild, `how-to-${name}`);
+			if (!channel) return "";
+
+			return `<#${channel.id}>`;
+		};
+
+		announce("bot-commands", {
+			content: `Hey there, <@${member.id}>! It seems like you don't have any roles. Make sure to update your nickname if you have not already`,
+			embed: formatEmbed({
+				title: `Welcome to the Server!`,
+				fields: [
+					{
+						name: "Getting Familiar With The Server",
+						value: oneLine(`If you are unfamiliar with the server,
+										make sure to read the how-to channels (${get("roles")}, ${get("commands")}, ${get("notifications")})`),
+					},
+					{
+						name: "Obtaining Roles to Gain Access to Channels",
+						value: oneLine(`You can assign yourself some roles using this [website](https://discord.ltseng.me/)
+										or using the \`!role\` command. To view a list of all assignable roles,
+										you can use the \`!roles\` command.`),
+					},
+				],
+			}),
+		});
+	}, 1000 * 60 * 2);
+
+	setTimeout(async () => {
+		let updated;
+		try {
+			updated = await member.guild.members.fetch({ user: id, force: true });
+		} catch (e) {
+			return;
+		}
+
+		if (updated.nickname === "real name please") {
+			announce(
+				"bot-commands",
+				oneLine(`<@${member.id}> you still have not updated your nickname.
+						Here are some steps if you are lost: 
+						(**Desktop**) Here are some Please type Click on \`UMass CICS Community\`
+						in bold in the top left of your screen.
+						Press \`Change Nickname\`, enter your identifier, and \`Save\`.
+						(**Mobile**) Swipe to the right to display your sever list.
+						Press the three vertically aligned dots next to \`UMass CICS Community\`.
+						Press \`Change Nickname\`, enter your identifier, and \`Save\`.`),
+			);
+		}
+	}, 1000 * 60 * 5);
+}
+
+export function login(token: string): Promise<void> {
+	return new Promise<void>((res, rej) => {
+		client.on("ready", () => res());
+
+		client.login(token).catch(error => rej(error));
+	}).then(async () => {
+		console.log(`Logged in as ${client.user?.tag}`);
+
+		await client.user?.setActivity({
+			type: "LISTENING",
+			name: "!help",
+		});
+
+		const guild = await client.guilds.fetch(DISCORD_GUILD_ID);
+		const botMember = await guild.members.fetch(client.user!);
+		if (!botMember.permissions.has("ADMINISTRATOR")) {
+			console.error("[SERVER] Insignificant permissions in guild.");
+			process.exit(-1);
+		}
+
+		const sendChannel = async (name: string, embeds: MessageEmbed | string | Array<MessageEmbed | string>) => {
+			let channel;
+			if ((channel = findChannel(guild, name))) {
+				await channel.bulkDelete(20);
+				if (embeds instanceof Array) {
+					for (const embed of embeds) await channel.send(embed);
+				} else {
+					await channel.send(embeds);
+				}
+			}
+		};
+
+		if (process.env["SEND_MESSAGES"]) {
+			await sendChannel("rules", DISCORD_RULES);
+			await sendChannel("how-to-roles", ROLES_TUTORIAL);
+			await sendChannel("how-to-notifications", NOTIFICATION_TUTORIALS);
+		}
+
+		client.on("message", message);
+		client.on("guildMemberAdd", guildMemberAdd);
+	});
+}
