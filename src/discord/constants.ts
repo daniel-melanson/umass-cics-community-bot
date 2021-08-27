ec1e778648f687022a9a45e1af9e83c84467b921
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-08-27 13:48:19 -0400

    feat(index): add calendar announcemnt cron jobs

---
+++ src/discord/constants.ts
@@ -0,0 +1,7 @@
+export const COLLEGE_NAME = "University of Massachusetts Amherst College of Information and Computer Sciences";
+
+export const UMASS_MAROON = 8594214;
+
+export const OWNER_ID = process.env["DISCORD_OWNER_ID"];
+
+export const CONTACT_MESSAGE = `Please contact <@${OWNER_ID}>.`;
---
+++ src/discord/formatting.ts
@@ -0,0 +1,60 @@
+import {
+  ColorResolvable,
+  EmbedFieldData,
+  FileOptions,
+  MessageAttachment,
+  User,
+  MessageEmbedOptions,
+  MessageEmbedFooter,
+  MessageEmbedAuthor,
+  MessageEmbedThumbnail,
+  MessageEmbedVideo,
+  MessageEmbed,
+} from "discord.js";
+
+import { UMASS_MAROON } from "#discord/constants";
+
+interface EmbedOptions {
+  title?: string;
+  description?: string;
+  url?: string;
+  timestamp?: boolean;
+  color?: ColorResolvable;
+  fields?: Array<EmbedFieldData>;
+  files?: Array<MessageAttachment | string | FileOptions>;
+  author?: User | MessageEmbedAuthor;
+  thumbnail?: Partial<MessageEmbedThumbnail> & { proxy_url?: string };
+  image?: string;
+  video?: Partial<MessageEmbedVideo> & { proxy_url?: string };
+  footer?: Partial<MessageEmbedFooter> & {
+    icon_url?: string;
+    proxy_icon_url?: string;
+  };
+}
+
+export function formatEmbed(opts: EmbedOptions): MessageEmbed {
+  const generated: MessageEmbedOptions = {
+    title: opts.title,
+    url: opts.url,
+    description: opts.description,
+    timestamp: opts.timestamp ? new Date() : undefined,
+    color: opts.color || UMASS_MAROON,
+    image: {
+      url: opts.image,
+    },
+    fields: opts.fields,
+    footer: opts.footer,
+  };
+
+  const author = opts.author;
+  if (author instanceof User) {
+    generated.author = {
+      name: author.tag,
+      iconURL: author.avatarURL({ dynamic: true }) || undefined,
+    };
+  } else if (author) {
+    generated.author = author;
+  }
+
+  return new MessageEmbed(generated);
+}
