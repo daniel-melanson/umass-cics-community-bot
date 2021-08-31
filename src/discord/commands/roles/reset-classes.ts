8594a9c9efd4626607b55f358e12a08341a41d43
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-08-31 11:47:26 -0400

    feat(discord): add reset-classes command

--- src/discord/commands/roles/reset-classes.ts
+++ src/discord/commands/roles/reset-classes.ts
@@ -1,11 +1,38 @@
+import { warn } from "../../../shared/logger";
 import { SlashCommandBuilder } from "../../builders/SlashCommandBuilder";
+import { isClass } from "../../roles";
+import { CommandError } from "../CommandError";
 
 export default new SlashCommandBuilder()
   .setName("reset-classes")
-  .setDescription("Removes all course related roles from a user.")
+  .setDescription("Remove all your course related roles.")
   .setGroup("Roles")
   .setDetails("")
   .addExamples(["/reset-classes"])
-  .setCallback(interaction => {
-    return interaction.reply("Not implemented.");
+  .setCallback(async interaction => {
+    const guild = interaction.guild!;
+    const guildMember = await guild.members.fetch(interaction.user.id);
+    const userRoleManager = guildMember.roles;
+    const roleCollection = userRoleManager.cache.mapValues(role => {
+      return { id: role.id, name: role.name };
+    });
+
+    const courseRoleCollection = roleCollection.filter(role => isClass(role.name));
+
+    if (courseRoleCollection.size <= 0) {
+      return interaction.reply("You do not have any course related roles to remove.");
+    }
+
+    try {
+      await userRoleManager.set(roleCollection.difference(courseRoleCollection).map(role => role.id));
+    } catch (e) {
+      warn("COMMAND", "role command unable to update permissions for " + interaction.user.id, e);
+      throw new CommandError("I'm sorry, I encountered an error while trying to update your roles.");
+    }
+
+    return interaction.reply(
+      `You no longer have the following ${courseRoleCollection.size > 1 ? "roles" : "role"}: ${courseRoleCollection
+        .map(role => role.name)
+        .join(", ")}`,
+    );
   });
