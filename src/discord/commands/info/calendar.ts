0f44b549c1042e7161d37d5046a08344612b1996
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-09-02 20:12:38 -0400

    feat(discord): add calendar command

--- src/discord/commands/info/calendar.ts
+++ src/discord/commands/info/calendar.ts
@@ -1,3 +1,6 @@
+import { MessageEmbedBuilder } from "discord/builders/MessageEmbedBuilder";
+import { getInSessionSemester } from "../../../umass/calendar";
+import { capitalize } from "../../../shared/stringUtil";
 import { SlashCommandBuilder } from "../../builders/SlashCommandBuilder";
 
 export default new SlashCommandBuilder()
@@ -5,6 +8,24 @@ export default new SlashCommandBuilder()
   .setDescription("Lists out academic events for the current in-session semester.")
   .setGroup("Information")
   .setDetails("")
-  .setCallback(interaction => {
-    return interaction.reply("Not implemented.");
+  .setCallback(async interaction => {
+    let semester;
+    try {
+      semester = await getInSessionSemester();
+    } catch (e) {
+      return interaction.reply("I had some trouble while trying to connect to the database.");
+    }
+
+    if (!semester) return interaction.reply("we are currently not in session.");
+
+    return interaction.reply({
+      embeds: [
+        new MessageEmbedBuilder({
+          title: `Academic Calendar for ${capitalize(semester.season)} ${semester.year}`,
+          description: semester.events.reduce((prev, current) => {
+            return prev + `**${current.date.toLocaleDateString()}**: ${current.description}\n`;
+          }, ""),
+        }),
+      ],
+    });
   });
