6ebc6c238e3f34b0618b50b414985391eaabeb57
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-09-03 19:19:48 -0400

    feat(discord): add what-is command

--- src/discord/commands/info/what-is.ts
+++ src/discord/commands/info/what-is.ts
@@ -1,4 +1,45 @@
+import { CommandInteraction, Message, MessageComponentInteraction, TextChannel } from "discord.js";
+import { MessageEmbedBuilder } from "../../builders/MessageEmbedBuilder";
+import { Course } from "../../../umass/types";
+import { oneLine, splitCamelCase } from "../../../shared/stringUtil";
+import { searchCourses, SearchResult } from "../../../umass/courses";
 import { SlashCommandBuilder } from "../../builders/SlashCommandBuilder";
+import { replyAndListenForButtonInteraction } from "../actions";
+
+const ignoredKeys = new Set(["id", "title", "website", "description", "subject", "number"]);
+
+async function doReply(interaction: CommandInteraction | MessageComponentInteraction, course: Course) {
+  const fields = [];
+
+  for (const [key, value] of Object.entries(course)) {
+    if (!ignoredKeys.has(key) && value && !key.startsWith("_")) {
+      fields.push({
+        name: splitCamelCase(key),
+        value: value instanceof Array ? value.join(", ") : value,
+      });
+    }
+  }
+
+  let reply = await interaction.reply({
+    embeds: [
+      new MessageEmbedBuilder({
+        title: `${course.id}: ${course.title}`,
+        url: course.website,
+        description: course.description,
+        fields,
+      }),
+    ],
+    fetchReply: true,
+  });
+
+  if (!(interaction.channel as TextChannel).name.match(/bot-commands/)) {
+    if (!("delete" in reply)) {
+      reply = await interaction.channel!.messages.fetch(reply.id);
+    }
+
+    setTimeout(() => (reply as Message).delete(), 30000);
+  }
+}
 
 export default new SlashCommandBuilder()
   .setName("what-is")
@@ -7,6 +48,28 @@ export default new SlashCommandBuilder()
   .setDetails("")
   .addExamples(["/what-is course: CS 187"])
   .addStringOption(option => option.setName("course").setDescription("The course to search for.").setRequired(true))
-  .setCallback(interaction => {
-    return interaction.reply("Not implemented.");
+  .setCallback(async interaction => {
+    const options = interaction.options;
+
+    let search: SearchResult;
+    try {
+      search = await searchCourses(options.getString("course", true));
+    } catch (e) {
+      console.log("[DATABASE]", e);
+      return interaction.reply("I encountered an error while attempting this query. Try again later.");
+    }
+
+    if (search.error) {
+      return interaction.reply(search.error);
+    } else if (search.result.length === 1) {  
+      doReply(interaction, search.result[0]);
+    } else {
+      replyAndListenForButtonInteraction(
+        interaction,
+        oneLine(`I was unable to narrow down your search to a single course.
+					Which one of the following did you mean?`),
+        search.result.map(x => x.id),
+        int => doReply(int, search.result.find(c => c.id === int.customId)!),
+      );
+    }
   });
