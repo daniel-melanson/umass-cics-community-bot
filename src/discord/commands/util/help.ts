f5c33a1f319caec0de40013c7e455878716069a9
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-09-09 16:43:45 -0400

    chore(discord): remove help command

--- src/discord/commands/util/help.ts
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from "../../builders/SlashCommandBuilder";
-
-export default new SlashCommandBuilder()
-  .setName("help")
-  .setDescription("Provides general information about the bot and available commands.")
-  .setCallback(interaction => {
-    return interaction.reply("Not implemented.");
-  });
