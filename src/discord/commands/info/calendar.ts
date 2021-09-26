7c79a6f0a56931b4fb2584a138a79c9e9d3b2e70
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2021-09-26 11:49:47 -0400

    feat(discord): allow user to choose semester

--- src/discord/commands/info/calendar.ts
+++ src/discord/commands/info/calendar.ts
@@ -7,6 +7,7 @@ import { MessageEmbedBuilder } from "#discord/classes/MessageEmbedBuilder";
 import { SlashCommandBuilder } from "#discord/classes/SlashCommandBuilder";
 import { CommandError } from "#discord/classes/CommandError";
 import { createChoiceListener } from "../createChoiceListener";
+import { CommandInteraction } from "discord.js";
 
 function makeSemesterEmbed(semester: Semester) {
   return new MessageEmbedBuilder({
@@ -17,57 +18,68 @@ function makeSemesterEmbed(semester: Semester) {
   });
 }
 
+function createSemesterChoiceListener(interaction: CommandInteraction, semesters: ReadonlyArray<Semester>) {
+  createChoiceListener(
+    interaction,
+    "Which semester would you like to see?",
+    semesters.map(semester => {
+      const embed = makeSemesterEmbed(semester);
+      return {
+        name: `${capitalize(semester.season)} ${semester.year}`,
+        onChoose: () => embed,
+      };
+    }),
+  );
+}
+
 export default new SlashCommandBuilder()
   .setName("calendar")
   .setDescription("Lists out academic events for the current in-session semester.")
   .setGroup("Information")
   .setDetails(
-    oneLine(`On a weekly basis, a child process will scrape the following [webpage](https://www.umass.edu/registrar/calendars/academic-calendar)
-    and store academic calender information in a database. This command will query that database and attempt to get the academic calendar of the
-    current in-session semester. If there are no in-session semesters, the command will fallback and fetch semesters that have not completed.
-    A semester is considered in-session if the current date is between the first and last day of classes. A semester is considered complete
-    if there are no events left in the semester. As an example, a semester that is currently in finals week is not in-session and incomplete.`),
+    oneLine(`This command replies with an embed listing out all the dates of a semester.
+    When \`select\` is true, the user will be able to choose from any available semester.
+    Otherwise, this command will attempt to determine the most suitable semester to display.
+    If the there is a current semester in session, that semester will be displayed.
+    If we are between two semesters (e.i Winter session starts before Fall grades are due),
+    then the user will be able to choose between the two.`),
+  )
+  .addBooleanOption(option =>
+    option.setName("select").setDescription("Manually select which calendar to see.").setRequired(false),
   )
   .setCallback(async interaction => {
+    if (interaction.options.getBoolean("select")) {
+      return createSemesterChoiceListener(interaction, getSemesters());
+    }
+
     const semester = getInSessionSemester();
-    if (!semester) {
-      let semesters = getCurrentSemesters();
-
-      if (semesters.length === 1) return makeSemesterEmbed(semesters[0]);
-      else if (semesters.length === 0) {
-        const now = Date.now();
-        semesters = getSemesters().filter(sem => sem.startDate.valueOf() > now);
-        if (semesters.length === 0)
-          throw new CommandError(
-            oneLine(`I'm sorry. It seems that I do not have the next semester in my database. You can find calender information here: 
-              https://www.umass.edu/registrar/calendars/academic-calendar`),
-          );
-
-        let closest = semesters[0];
-        let closestDiff = closest.startDate.valueOf() - now;
-        for (let i = 1; i < semesters.length; i++) {
-          const diff = semesters[i].startDate.valueOf() - now;
-          if (diff < closestDiff) {
-            closest = semesters[i];
-            closestDiff = diff;
-          }
-        }
-
-        return makeSemesterEmbed(closest);
-      } else {
-        createChoiceListener(
-          interaction,
-          "Which semester would you like to see?",
-          semesters.map(semester => {
-            const embed = makeSemesterEmbed(semester);
-            return {
-              name: embed.title!,
-              onChoose: () => embed,
-            };
-          }),
-        );
-      }
-    } else {
+    if (semester) {
       return makeSemesterEmbed(semester);
+    }
+
+    const currentSemesters = getCurrentSemesters();
+    if (currentSemesters.length === 1) return makeSemesterEmbed(currentSemesters[0]);
+    if (currentSemesters.length === 0) {
+      const now = Date.now();
+      const semesters = getSemesters().filter(sem => sem.startDate.valueOf() > now);
+      if (semesters.length === 0)
+        throw new CommandError(
+          oneLine(`I'm sorry. It seems that I do not have the next semester in my database. You can find calender information here: 
+              https://www.umass.edu/registrar/calendars/academic-calendar`),
+        );
+
+      let closest = semesters[0];
+      let closestDiff = closest.startDate.valueOf() - now;
+      for (let i = 1; i < semesters.length; i++) {
+        const diff = semesters[i].startDate.valueOf() - now;
+        if (diff < closestDiff) {
+          closest = semesters[i];
+          closestDiff = diff;
+        }
+      }
+
+      return makeSemesterEmbed(closest);
+    } else {
+      return createSemesterChoiceListener(interaction, currentSemesters);
     }
   });
