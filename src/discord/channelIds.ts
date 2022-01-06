b3a36d3bd87aba65ba8d9d8f00a492200da38aaa
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-01-06 16:52:54 -0500

    fix: ids to snowflakes

--- src/discord/channelIds.ts
+++ src/discord/channelIds.ts
@@ -1,12 +1,12 @@
 export default {
   prod: {
-    general: 642491654967787572,
-    university: 696362051420815390,
-    "bot-log": 696131286674309201,
-    "bot-commands": 642491740598566922,
-    "cics-events": 696362294749167667,
-    "how-to-roles": 794227162554302464,
-    "how-to-notifications": 794230704195764224,
+    general: "642491654967787572",
+    university: "696362051420815390",
+    "bot-log": "696131286674309201",
+    "bot-commands": "642491740598566922",
+    "cics-events": "696362294749167667",
+    "how-to-roles": "794227162554302464",
+    "how-to-notifications": "794230704195764224",
   },
   dev: {},
 };
