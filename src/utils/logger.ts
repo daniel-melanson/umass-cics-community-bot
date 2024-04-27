9f1a31babcbb55f34c8d2e078dc12c5de62f38ab
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2024-04-27 13:37:50 -0400

    fix: logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,13 @@
 import pino from "pino";
-import { $ } from "bun";
 
 const transport = pino.transport({
   target: "pino-pretty",
   options: {
     destination: 1,
-    level: process.env.NODE_ENV === "production" ? "info" : "trace",
   },
 });
 
-export const logger = pino(transport);
+export const logger = pino(
+  { level: process.env.NODE_ENV === "production" ? "info" : "trace" },
+  transport,
+);
